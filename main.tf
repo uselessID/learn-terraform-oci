ee27df750fd7c47870fbe0502c1901994470249d
uselessID <11247793+uselessID@users.noreply.github.com>
2022-02-16 11:06:21 +0100

    Change VCN subnet name.

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "oci_core_subnet" "dev" {
   vcn_id                      = oci_core_vcn.internal.id
   cidr_block                  = "172.16.0.0/24"
   compartment_id              = var.compartment_ocid
-  display_name                = "Dev subnet 1"
+  display_name                = "Dev subnet"
   prohibit_public_ip_on_vnic  = true
   dns_label                   = "dev"
 }
