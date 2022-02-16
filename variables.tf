9500ae3819d7b6805ae26e1b94a6eee02d10df14
uselessID <11247793+uselessID@users.noreply.github.com>
2022-02-16 11:23:25 +0100

    Add description to variables.

--- variables.tf
+++ variables.tf
@@ -1,6 +1,10 @@
 variable "region" {
+  description = "region where you have OCI tenancy"
+  type        = string
   default = "eu-frankfurt-1"
 }
 
 variable "compartment_ocid" {
+  description = "OCID from your tenancy page"
+  type        = string
 }
