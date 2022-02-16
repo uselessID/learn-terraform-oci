5c4a89a0c18641f816b20cb4cbb29be281bef993
uselessID <11247793+uselessID@users.noreply.github.com>
2022-02-16 10:59:37 +0100

    Add a VCN subnet resource.

--- main.tf
+++ main.tf
@@ -18,3 +18,12 @@ resource "oci_core_vcn" "internal" {
   compartment_id = var.compartment_ocid
   display_name   = "My internal VCN"
 }
+
+resource "oci_core_subnet" "dev" {
+  vcn_id                      = oci_core_vcn.internal.id
+  cidr_block                  = "172.16.0.0/24"
+  compartment_id              = var.compartment_ocid
+  display_name                = "Dev subnet 1"
+  prohibit_public_ip_on_vnic  = true
+  dns_label                   = "dev"
+}
